ffcfdf396de41e5ee1b752452b0c0518e864e3f5
zerotwo02255 <ankuwizard5@gmail.com>
2026-03-03 19:38:29 +0545

    pyton

---
+++ init.py
@@ -0,0 +1,10 @@
+class jeff:
+    def __init__(self,name,age):
+        self.name=name
+        self.age=age
+
+p1=jeff("je",20)
+p2=jeff("mscks",200)
+
+print(p1.name,p2.age)
+print(p2.name,p1.age)
---
+++ oop.py
@@ -0,0 +1,10 @@
+class jeff:
+    x=10
+    y=20
+
+p1=jeff()
+print(jeff.x)
+print(jeff.y)
+p2=jeff()
+print(jeff.x)
+del p2
